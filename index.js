3c58bc3feee80f34d5c53a98557c33ff6b457661
Василий <58366884+lthonny@users.noreply.github.com>
2021-11-19 21:27:19 +0300

    init

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(process.cwd(), `env/.env.${process.env.NODE_ENV}`) });
-require("./app/passport");
-
-const express = require("express");
-const app = express();
-const passport = require("passport");
-
-const cors = require("cors");
-const cookieSession = require("cookie-session");
-const cookieParser = require('cookie-parser');
-
+const cors = require('cors');
+const sequelize = require('./db');
+const models = require('./app/models/models');
+const express = require('express');
 const router = require('./app/routes/index');
-const authRoute = require("./app/routes/google");
 
-// app.use(
-//   cookieSession({
-//     name: "session",
-//     keys: ["lama"],
-//     maxAge: 24 * 60 * 60 * 100
-//   })
-// );
+const PORT = process.env.PORT || 5000;
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-app.use(
-  cors({
-    origin: "http://localhost:4200",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
+app.use('/api', router);
 
-app.use(router);
-// app.use("/auth", authRoute);
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running!`);
-});
+start();
+
