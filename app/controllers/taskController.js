d4cc605f681905e64cf9126542adf614f2e5c837
Василий <58366884+lthonny@users.noreply.github.com>
2021-12-09 18:16:26 +0300

    Update taskController.js

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -4,9 +4,7 @@ const dragAndDrop = require('../services/dragDrop');
 class TaskController {
   async createTask(req, res) {
     try {
-      // не работает проверка
       const access = await taskService.authorizeAccess(Number(req.decoded.id), req.body.data.board_id);
-      // не работает проверка
       if (!access) {
         res.sendStatus(403);
       }
